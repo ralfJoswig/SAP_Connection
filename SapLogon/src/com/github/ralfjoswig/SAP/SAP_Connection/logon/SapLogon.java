1f7add99d199f1744080c361cf14f6ab83b45339
Ralf Joswig <ralfjoswig@gmx.de>
2018-05-31 22:58:32 +0200

    s

--- SapLogon/src/com/github/ralfjoswig/SAP/SAP_Connection/logon/SapLogon.java
+++ SapLogon/src/com/github/ralfjoswig/SAP/SAP_Connection/logon/SapLogon.java
@@ -10,7 +10,7 @@
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, see <http://www.gnu.org/licenses/> 
  */
-package com.github.ralfjoswig.SAP.SAP_Connection.logon;
+package com.github.ralfjoswig.SAP.SAP_Connection.logon; 
 
 /**
  * @author Ralf Joswig 
